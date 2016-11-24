25a7a8972ab985e12dacfcc4251f9992366d98e5
Björn Grüning <bjoern@gruenings.eu>
2016-11-24 11:56:09 +0100

    Change links

--- recipes/r-rgl/build.sh
+++ recipes/r-rgl/build.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
-cp /usr/lib64/libGL.so $PREFIX/lib
-ln -s $PREFIX/lib/libGL.so $PREFIX/lib/libGL.so.1.2.0
-ln -s $PREFIX/lib/libGL.so $PREFIX/lib/libGL.so.1
-
 # R refuses to build packages that mark themselves as Priority: Recommended
 mv DESCRIPTION DESCRIPTION.old
 grep -v '^Priority: ' DESCRIPTION.old > DESCRIPTION
 
 $R CMD INSTALL --build .
+
+cp /usr/lib64/libGL.so $PREFIX/lib
+cd $PREFIX/lib
+ln -s ./libGL.so ./libGL.so.1.2.0
+ln -s ./libGL.so ./libGL.so.1
+
 
 # Add more build steps here, if they are necessary.
 
