3d5db63b5b4935da67c08248c57b8d4bf7934061
Roman Valls Guimera <brainstorm@nopcode.org>
2016-11-24 12:46:48 +0000

    Switched to main setup.sh script, added VagRent dependency [ci skip]

--- recipes/perl-grass/build.sh
+++ recipes/perl-grass/build.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
-cpanm -i --notest .
+# Extracted from from grass/setup.sh
+
+#cpanm --mirror http://cpan.metacpan.org --notest -l $PREFIX --installdeps .
+
+#perl Makefile.PL INSTALL_BASE=$PREFIX && \
+#make && \
+#make test && \
+#make install
+
+./setup.sh $PREFIX
