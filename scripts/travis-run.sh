6e77ff7ec83c72e0885d6225401dac5cae2c5e38
Bjoern Gruening <bjoern.gruening@gmail.com>
2016-11-26 06:49:45 +0100

    try

--- scripts/travis-run.sh
+++ scripts/travis-run.sh
@@ -16,7 +16,7 @@ else
     USE_DOCKER=""
 fi
 
-set -x; bioconda-utils build recipes config.yml $USE_DOCKER $BIOCONDA_UTILS_ARGS; set +x;
+
 
 # build package documentation
 if [[ $TRAVIS_OS_NAME = "linux" ]]
